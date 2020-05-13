b5d379c7ef1860311809e3866c2a0e3fb93e33b2
july89vignesh <63089305+july89vignesh@users.noreply.github.com>
2020-05-13 12:09:54 +0530

    Component and style changes

--- src/App.js
+++ src/App.js
@@ -2,92 +2,30 @@
 import React, { Component } from "react";
 import "./styles.css";
 
-const list = [
-  {
-    id: 1,
-    name: "John Lennon",
-    votes: 0
-  },
-  {
-    id: 2,
-    name: "Paul McCartney",
-    votes: 0
-  },
-  {
-    id: 3,
-    name: "George Harrison",
-    votes: 0
-  },
-  {
-    id: 4,
-    name: "Ringo Starr",
-    votes: 0
-  }
-];
-
 class App extends Component {
-  state = {
-    members: []
-  };
-
-  componentDidMount() {
-    this.setState({ members: list });
-  }
-
-  handleEvent = memberId => {
-    const updatedList = this.state.members.map(member => {
-      if (member.id === memberId) {
-        return Object.assign({}, member, {
-          votes: member.votes + 1
-        });
-      } else {
-        return member;
-      }
-    });
-
-    this.setState({
-      members: updatedList.sort((a, b) => b.votes - a.votes)
-    });
-  };
-
-  render() {
-    return this.state.members.map(member => (
-      <Beatle
-        key={member.id}
-        id={member.id}
-        name={member.name}
-        votes={member.votes}
-        onVote={this.handleEvent}
-      />
-    ));
-  }
-}
-
-class Beatle extends Component {
   handleClick = () => this.props.onVote(this.props.id);
 
   render() {
     return (
       <div className="row">
         <div className="container">
-          <div className="col-md-3 col-sm-3 prof-pic">
-            <img
-              src="https://www.w3schools.com/howto/img_avatar.png"
-              alt="Avatar"
-            />
+          <div className="col-md-3 col-sm-3 col-xs-12 prof-pic">
+            <img src={this.props.profile} alt="Avatar" />
           </div>
-          <div className="col-md-9 col-sm-9 candidate-detail">
+          <div className="col-md-9 col-sm-9 col-xs-12 candidate-detail">
             <div className="details">
-              <p>{this.props.name}</p>
+              <h5>{this.props.name}</h5>
             </div>
             <div className="desc">
               <p>Text Goes here</p>
             </div>
             <div className="votes">
-              No. of votes - {this.props.votes}
-              <a href="#" onClick={this.handleClick}>
-                Click here to vote
-              </a>
+              <p className="vote-btn">No. of votes</p> - {this.props.votes}
+              <p>
+                <a href="#" onClick={this.handleClick}>
+                  Click here to vote
+                </a>
+              </p>
             </div>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,71 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const list = [
+  {
+    id: 1,
+    name: "Pat Mclrean",
+    votes: 0,
+    profile: "https://www.w3schools.com/howto/img_avatar.png"
+  },
+  {
+    id: 2,
+    name: "Claretia Mary",
+    votes: 0,
+    profile: "https://www.w3schools.com/howto/img_avatar2.png"
+  },
+  {
+    id: 3,
+    name: "Williams Georgia",
+    votes: 0,
+    profile: "https://www.w3schools.com/w3images/avatar5.png"
+  },
+  {
+    id: 4,
+    name: "Matt Alberta",
+    votes: 0,
+    profile: "https://www.w3schools.com/w3images/avatar6.png"
+  }
+];
+
+class VoteCount extends Component {
+  state = {
+    members: []
+  };
+
+  componentDidMount() {
+    this.setState({ members: list });
+  }
+
+  handleEvent = memberId => {
+    const updatedList = this.state.members.map(member => {
+      if (member.id === memberId) {
+        return Object.assign({}, member, {
+          votes: member.votes + 1
+        });
+      } else {
+        return member;
+      }
+    });
+
+    this.setState({
+      members: updatedList.sort((a, b) => b.votes - a.votes)
+    });
+  };
+
+  render() {
+    return this.state.members.map(member => (
+      <App
+        key={member.id}
+        id={member.id}
+        name={member.name}
+        votes={member.votes}
+        profile={member.profile}
+        onVote={this.handleEvent}
+      />
+    ));
+  }
+}
+
+ReactDOM.render(<VoteCount />, document.getElementById("root"));
